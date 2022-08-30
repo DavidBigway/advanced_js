218c87b556754eb93e4486b9933651369f997c77
David <bigway8@vk.com>
2022-08-31 00:48:36 +0300

    l4

--- src/js/main.js
+++ src/js/main.js
@@ -1,123 +1,270 @@
 const API =
   'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
-const cartPopup = document.querySelector('.cart')
-cartPopup.addEventListener('click', () => {
-  document.querySelector('.cart-popup').classList.toggle('active')
-})
-class ProductsList {
-  constructor(container = '.products', total = '.total-good-price') {
+
+// let getRequest = (url) => {
+//     return new Promise((resolve, reject) => {
+//         let xhr = new XMLHttpRequest();
+//         // window.ActiveXObject -> xhr = new ActiveXObject()
+//         xhr.open("GET", url, true);
+//         xhr.onreadystatechange = () => {
+//             if(xhr.readyState === 4){
+//                 if(xhr.status !== 200){
+//                     reject('Error');
+//                 } else {
+//                     resolve(xhr.responseText);
+//                 }
+//             }
+//         };
+//         xhr.send();
+//     })
+// };
+//
+// getRequest('tel.json').then(data => {
+//
+// })
+
+class List {
+  constructor(url, container, list = list2) {
     this.container = container
-    this.total = total
+    this.list = list
+    this.url = url
     this.goods = []
-    this._fetchProducts().then((data) => {
-      this.goods = data
-      this.render()
-    })
+    this.allProducts = []
+    this.filtered = []
+    this._init()
   }
-  _fetchProducts() {
-    return fetch(`${API}/catalogData.json`)
-      .then((res) => res.json())
-      .catch((error) => console.log(error))
+  getJson(url) {
+    return fetch(url ? url : `${API + this.url}`)
+      .then((result) => result.json())
+      .catch((error) => {
+        console.log(error)
+      })
   }
-  _totalGoodsPrice() {
-    let sum = 0
-    for (let i = 0; i < this.goods.length; i++) {
-      sum += Number(this.goods[i].price)
-    }
-    return sum
+  handleData(data) {
+    this.goods = [...data]
+    this.render()
+  }
+  calcSum() {
+    return this.allProducts.reduce((accum, item) => (accum += item.price), 0)
   }
   render() {
     const block = document.querySelector(this.container)
-    const total = document.querySelector(this.total)
     for (let product of this.goods) {
-      const item = new ProductItem(product)
-      block.insertAdjacentHTML('beforeend', item.render())
+      const productObj = new this.list[this.constructor.name](product)
+      console.log(productObj)
+      this.allProducts.push(productObj)
+      block.insertAdjacentHTML('beforeend', productObj.render())
     }
-    total.insertAdjacentHTML('beforeend', this._totalGoodsPrice())
   }
-}
-
-class ProductItem {
-  constructor(
-    product,
-    img = 'https://welcome.setantasports.com/wp-content/uploads/2022/03/placeholder.png'
-  ) {
-    this.id = product.id_product
-    this.title = product.product_name
-    this.price = product.price
-    this.img = product.img || img
-  }
-  render() {
-    return `
-  <div data-id="${this.id}" class="product">
-      <img src="${this.img}" alt="${this.title}"/>
-      <h2>${this.title}</h2>
-      <p>${this.price}</p>
-      <button class="add-to-cart__btn">Купить</button>
-  </div>
-  `
-  }
-}
-let list = new ProductsList()
-
-class Cart {
-  constructor(cart = '.cart-popup', count = '.count', total = 0) {
-    this.cart = cart
-    this.cartList = []
-    this.count = count
-    this.total = total
-    this._fetchCart().then((data) => {
-      this.cartList = data
-      this.render()
+  filter(value) {
+    const regexp = new RegExp(value, 'i')
+    this.filtered = this.allProducts.filter((product) =>
+      regexp.test(product.product_name)
+    )
+    this.allProducts.forEach((el) => {
+      const block = document.querySelector(
+        `.product-item[data-id="${el.id_product}"]`
+      )
+      if (!this.filtered.includes(el)) {
+        block.classList.add('invisible')
+      } else {
+        block.classList.remove('invisible')
+      }
     })
   }
-  // не понял, как их добавить в рендер, чтобы при клике запускался метод
-  // increment() {}
-  // decrement() {}
-  // removeCategory() {}
-  // clear() {}
-  // order() {}
-  _fetchCart() {
-    return fetch(`${API}/getBasket.json`)
-      .then((res) => res.json())
-      .catch((error) => console.log(error))
-  }
-  render() {
-    const cart = document.querySelector(this.cart)
-    for (let product of this.cartList.contents) {
-      const item = new CartItem(product)
-      cart.insertAdjacentHTML('beforeend', item.render())
-    }
+  _init() {
+    return false
   }
 }
-class CartItem {
-  constructor(
-    product,
-    img = 'https://welcome.setantasports.com/wp-content/uploads/2022/03/placeholder.png'
-  ) {
-    this.id = product.id_product
-    this.title = product.product_name
-    this.price = product.price
-    this.img = product.img || img
-    this.count = product.quantity || 0
-    this.total = product.quantity * product.price || 0
-    this.render()
+
+class Item {
+  constructor(el, img = 'https://placehold.it/200x150') {
+    this.product_name = el.product_name
+    this.price = el.price
+    this.id_product = el.id_product
+    this.img = img
   }
   render() {
-    return `
-    <div data-id="${this.id}" class="cart-item">
-      <img src="${this.img}" alt="${this.title}"/>
-      <div class="cart-item__info">
-        <h2>${this.title}</h2>
-        <p>${this.price}</p>
-      </div>
-      <div class="actions"><div class="pin increment">+</div>
-      <div class="count">${this.count}</div>
-      <div class="pin decrement">-</div></div>
-      <div class="total">${this.total}</div>
-      <div class="remove-category">Х</div>
-    </div>
-    `
+    return `<div class="product-item" data-id="${this.id_product}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.product_name}</h3>
+                    <p>${this.price} $</p>
+                    <button class="buy-btn"
+                    data-id="${this.id_product}"
+                    data-name="${this.product_name}"
+                    data-price="${this.price}">Купить</button>
+                </div>
+            </div>`
   }
 }
+
+class ProductsList extends List {
+  constructor(cart, container = '.products', url = '/catalogData.json') {
+    super(url, container)
+    this.cart = cart
+    this.getJson().then((data) => this.handleData(data))
+  }
+  _init() {
+    document.querySelector(this.container).addEventListener('click', (e) => {
+      if (e.target.classList.contains('buy-btn')) {
+        this.cart.addProduct(e.target)
+      }
+    })
+    document.querySelector('.search-form').addEventListener('submit', (e) => {
+      e.preventDefault()
+      this.filter(document.querySelector('.search-field').value)
+    })
+  }
+}
+
+class ProductItem extends Item {}
+
+class Cart extends List {
+  constructor(container = '.cart-block', url = '/getBasket.json') {
+    super(url, container)
+    this.getJson().then((data) => {
+      this.handleData(data.contents)
+    })
+  }
+  addProduct(element) {
+    this.getJson(`${API}/addToBasket.json`).then((data) => {
+      if (data.result === 1) {
+        let productId = +element.dataset['id']
+        let find = this.allProducts.find(
+          (product) => product.id_product === productId
+        )
+        if (find) {
+          find.quantity++
+          this._updateCart(find)
+        } else {
+          let product = {
+            id_product: productId,
+            price: +element.dataset['price'],
+            product_name: element.dataset['name'],
+            quantity: 1,
+          }
+          this.goods = [product]
+          this.render()
+        }
+      } else {
+        alert('Error')
+      }
+    })
+  }
+  removeProduct(element) {
+    this.getJson(`${API}/deleteFromBasket.json`).then((data) => {
+      if (data.result === 1) {
+        let productId = +element.dataset['id']
+        let find = this.allProducts.find(
+          (product) => product.id_product === productId
+        )
+        if (find.quantity > 1) {
+          find.quantity--
+          this._updateCart(find)
+        } else {
+          this.allProducts.splice(this.allProducts.indexOf(find), 1)
+          document.querySelector(`.cart-item[data-id="${productId}"]`).remove()
+        }
+      } else {
+        alert('Error')
+      }
+    })
+  }
+  _updateCart(product) {
+    let block = document.querySelector(
+      `.cart-item[data-id="${product.id_product}"]`
+    )
+    block.querySelector(
+      '.product-quantity'
+    ).textContent = `Quantity: ${product.quantity}`
+    block.querySelector('.product-price').textContent = `$${
+      product.quantity * product.price
+    }`
+  }
+  _init() {
+    document.querySelector('.btn-cart').addEventListener('click', () => {
+      document.querySelector(this.container).classList.toggle('invisible')
+    })
+    document.querySelector(this.container).addEventListener('click', (e) => {
+      if (e.target.classList.contains('del-btn')) {
+        this.removeProduct(e.target)
+      }
+    })
+  }
+}
+
+class CartItem extends Item {
+  constructor(el, img = 'https://placehold.it/50x100') {
+    super(el, img)
+    this.quantity = el.quantity
+  }
+  render() {
+    return `<div class="cart-item" data-id="${this.id_product}">
+            <div class="product-bio">
+            <img src="${this.img}" alt="Some image">
+            <div class="product-desc">
+            <p class="product-title">${this.product_name}</p>
+            <p class="product-quantity">Quantity: ${this.quantity}</p>
+        <p class="product-single-price">$${this.price} each</p>
+        </div>
+        </div>
+        <div class="right-block">
+            <p class="product-price">$${this.quantity * this.price}</p>
+            <button class="del-btn" data-id="${
+              this.id_product
+            }">&times;</button>
+        </div>
+        </div>`
+  }
+}
+const list2 = {
+  ProductsList: ProductItem,
+  Cart: CartItem,
+}
+
 let cart = new Cart()
+let products = new ProductsList(cart)
+products.getJson(`getProducts.json`).then((data) => products.handleData(data))
+
+const quotesText = `One: 'Hi Mary.'
+Two: 'Oh, hi.'
+One: 'How are you doing?'
+Two: 'I'm doing alright. How about you?'
+One: 'Not too bad. The weather is great isn't it?'
+Two: 'Yes. It's absolutely beautiful today.'
+One: 'I wish it was like this more frequently.'
+Two: 'Me too.'
+One: 'So where are you going now?'
+Two: 'I'm going to meet a friend of mine at the department store.'
+One: 'Going to do a little shopping?'
+Two: 'Yeah, I have to buy some presents for my parents.'
+One: 'What's the occasion?'
+Two: 'It's their anniversary.'
+One: 'That's great. Well, you better get going. You don't want to be late.'
+Two: 'I'll see you next time.'
+One: 'Sure. Bye.'`
+
+const re = /\n/gm
+const q = quotesText.replace(re, '<br/>')
+
+const quotesTextEl = document.querySelector('.quotes-text')
+const allQuotesEl = document.querySelectorAll('button')
+quotesTextEl.insertAdjacentHTML('beforeend', q)
+const allQuotes = (e) => {
+  quotesTextEl.innerHTML = ''
+  const datasetId = e.target.dataset.id
+  if (datasetId === 'reset') {
+    quotesTextEl.insertAdjacentHTML('beforeend', q)
+  }
+  if (datasetId === 'all-quotes') {
+    const s = q.replace(/'/gm, `"`)
+    quotesTextEl.insertAdjacentHTML('beforeend', s)
+  }
+  if (datasetId === 'none-apostrophe') {
+    const s = q.replace(/[ ]+\'|[']+\n/gm, `"`)
+    quotesTextEl.insertAdjacentHTML('beforeend', s)
+  }
+}
+
+allQuotesEl.forEach((elem) => elem.addEventListener('click', allQuotes))
