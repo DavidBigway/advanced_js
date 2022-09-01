baad75b99787fc31da10adeb60bb3ed5f16a0b6d
David <bigway8@vk.com>
2022-09-02 02:16:34 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,5 @@
-<<<<<<< HEAD
 const API =
   'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
-
-// let getRequest = (url) => {
-//     return new Promise((resolve, reject) => {
-//         let xhr = new XMLHttpRequest();
-//         // window.ActiveXObject -> xhr = new ActiveXObject()
-//         xhr.open("GET", url, true);
-//         xhr.onreadystatechange = () => {
-//             if(xhr.readyState === 4){
-//                 if(xhr.status !== 200){
-//                     reject('Error');
-//                 } else {
-//                     resolve(xhr.responseText);
-//                 }
-//             }
-//         };
-//         xhr.send();
-//     })
-// };
-//
-// getRequest('tel.json').then(data => {
-//
-// })
 
 class List {
   constructor(url, container, list = list2) {
@@ -269,30 +246,3 @@ const allQuotes = (e) => {
 }
 
 allQuotesEl.forEach((elem) => elem.addEventListener('click', allQuotes))
-=======
-const products = [
-  { id: 1, title: 'Notebook', price: '2000', img: 'src/img/1.png' },
-  { id: 2, title: 'mouse', price: '150', img: 'src/img/2.png' },
-  { id: 3, title: 'keyboard', price: '200', img: 'src/img/3.png' },
-  { id: 4, title: 'monitor', price: '1000', img: 'src/img/4.png' },
-]
-
-const renderProduct = (product) => {
-  const { title, price, img } = product
-  return `
-    <div class="product">
-        <img src="${img}"/>
-        <h2>${title}</h2>
-        <p>${price}</p>
-        <button>Купить</button>
-    </div>
-    `
-}
-const renderPage = (products) => {
-  const productsList = products.map((product) => renderProduct(product))
-  // появились из-за того, что мы записываем массив в виде строки ["div", "div", "div"] => "div", "div", "div"
-  // поэтому он заполняет наш элемент с запятыми. Join возвращает новую строку с символом, который мы укажем, тут ""
-  document.querySelector('.products').innerHTML = productsList.join('')
-}
-renderPage(products)
->>>>>>> ad85036e2e714dfaa94dc1246582fa158a7506c1
